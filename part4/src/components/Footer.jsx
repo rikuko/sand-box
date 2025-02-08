2d89e51bec0d18dbba11ef401c66ba19a7b82a06
Riku Koskinen <riku.i.koskinen@gmail.com>
2025-02-08 13:48:12 +0200

    pikku muutos footerin tyyyliin

--- part4/src/components/Footer.jsx
+++ part4/src/components/Footer.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 
 const Footer = () => {
+    return (
+        <div className='footer'>
+            <br />
+            <em>Note app, Department of Computer Science, University of Helsinki 2025</em>
+        </div>
+    )
 }
-return (
-    <div>
-        <div className='footer' />
-        <br />
-        <em>Note app, Department of Computer Science, University of Helsinki 2025</em>
-    </div>
-)
 
 export default Footer
