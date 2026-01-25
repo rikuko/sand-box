8b030b85daae1b20efda5303e11ce7b32d810626
Riku Koskinen <riku.i.koskinen@gmail.com>
2026-01-25 14:23:28 +0200

    Lint-config

--- part5/eslint.config.js
+++ part5/eslint.config.js
@@ -1,3 +1,6 @@
+/* https://eslint.org/ */
+/* https://eslint.style/rules */
+
 import js from '@eslint/js'
 import globals from 'globals'
 import react from 'eslint-plugin-react'
@@ -5,34 +8,61 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: [ 'dist' ] },
   {
-    files: ['**/*.{js,jsx}'],
+    files: [ '**/*.{js,jsx}' ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+        sourceType: 'module'
+      }
     },
     settings: { react: { version: '18.3' } },
     plugins: {
       react,
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      'react-refresh': reactRefresh
     },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
+
       'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
+      'react-refresh/only-export-components': [ 'warn', { allowConstantExport: true } ],
+      'react/jsx-closing-bracket-location': [ 'error', 'line-aligned' ],
+
+      'indent': [ 'error', 2 ],
+      'quotes': [ 'error', 'single' ],
+      'jsx-quotes': [ 'error', 'prefer-single' ],
+
+
+      'semi': [ 'error', 'never' ],
+      'linebreak-style': [ 'error', 'unix' ],
+      'array-bracket-newline': [ 'warn', 'consistent' ],
+      'array-bracket-spacing': [ 'warn', 'always' ],
+      'array-element-newline': [ 'warn', 'consistent' ],
+
+      'max-len': [ 'error', { 'code': 120 } ],
+      'object-curly-spacing': [ 'error', 'always' ],
+      'arrow-spacing': [ 'error', { before: true, after: true } ],
+
+      'comma-dangle': [ 'warn', 'never' ],
+      'comma-spacing': [ 'warn', { 'before': false, 'after': true } ],
+      'comma-style': [ 'error', 'last' ],
+      'lines-around-comment': [ 'error', { 'beforeBlockComment': true, 'beforeLineComment': true } ],
+      'multiline-comment-style': [ 'error', 'starred-block' ],
+
+      'no-unused-vars': [ 'error', { varsIgnorePattern: '^[A-Z_]' } ],
+      'no-trailing-spaces': 'error',
+      'no-console': 'off',
+
+      'react/prop-types': [ 'off', { ignore: [] } ],
+      eqeqeq: 'error'
+    }
+  }
 ]
